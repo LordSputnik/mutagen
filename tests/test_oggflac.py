1347f99a6fda057f08c5259dae5f64fd0d893999
Ben Ockmore <ben.sput@gmail.com>
2013-08-04 17:21:33 +0100

    Ogg Theora and Ogg FLAC support & tests.

---
+++ tests/test_oggflac.py
@@ -0,0 +1,99 @@
+import os
+import shutil
+import sys
+import tempfile
+import io
+
+from tests import add
+from tests.test_ogg import TOggFileType
+
+from mutagen.oggflac import OggFLAC, OggFLACStreamInfo, delete
+from mutagen.ogg import OggPage
+
+class TOggFLAC(TOggFileType):
+    Kind = OggFLAC
+
+    def setUp(self):
+        original = os.path.join("tests", "data", "empty.oggflac")
+        fd, self.filename = tempfile.mkstemp(suffix='.ogg')
+        os.close(fd)
+        shutil.copy(original, self.filename)
+        self.audio = OggFLAC(self.filename)
+
+    def test_vendor(self):
+        self.failUnless(
+            self.audio.tags.vendor.startswith("reference libFLAC"))
+        self.failUnlessRaises(KeyError, self.audio.tags.__getitem__, "vendor")
+
+    def test_streaminfo_bad_marker(self):
+        page = OggPage(open(self.filename, "rb")).write()
+        page = page.replace(b"fLaC", b"!fLa", 1)
+        self.failUnlessRaises(IOError, OggFLACStreamInfo, io.BytesIO(page))
+
+    def test_streaminfo_bad_version(self):
+        page = OggPage(open(self.filename, "rb")).write()
+        page = page.replace(b"\x01\x00", b"\x02\x00", 1)
+        self.failUnlessRaises(IOError, OggFLACStreamInfo, io.BytesIO(page))
+
+    def test_flac_reference_simple_save(self):
+        if not have_flac: return
+        self.audio.save()
+        self.scan_file()
+        value = os.system("flac --ogg -t {} 2> {}".format(self.filename, os.devnull))
+        self.failIf(value and value != NOTFOUND)
+
+    def test_flac_reference_really_big(self):
+        if not have_flac: return
+        self.test_really_big()
+        self.audio.save()
+        self.scan_file()
+        value = os.system("flac --ogg -t {} 2> {}".format(self.filename, os.devnull))
+        self.failIf(value and value != NOTFOUND)
+
+    def test_module_delete(self):
+        delete(self.filename)
+        self.scan_file()
+        self.failIf(OggFLAC(self.filename).tags)
+
+    def test_flac_reference_delete(self):
+        if not have_flac: return
+        self.audio.delete()
+        self.scan_file()
+        value = os.system("flac --ogg -t {} 2> {}".format(self.filename, os.devnull))
+        self.failIf(value and value != NOTFOUND)
+
+    def test_flac_reference_medium_sized(self):
+        if not have_flac: return
+        self.audio["foobar"] = "foobar" * 1000
+        self.audio.save()
+        self.scan_file()
+        value = os.system("flac --ogg -t {} 2> {}".format(self.filename, os.devnull))
+        self.failIf(value and value != NOTFOUND)
+
+    def test_flac_reference_delete_readd(self):
+        if not have_flac: return
+        self.audio.delete()
+        self.audio.tags.clear()
+        self.audio["foobar"] = "foobar" * 1000
+        self.audio.save()
+        self.scan_file()
+        value = os.system("flac --ogg -t {} 2> {}".format(self.filename, os.devnull))
+        self.failIf(value and value != NOTFOUND)
+
+    def test_not_my_ogg(self):
+        fn = os.path.join('tests', 'data', 'empty.ogg')
+        self.failUnlessRaises(IOError, type(self.audio), fn)
+        self.failUnlessRaises(IOError, self.audio.save, fn)
+        self.failUnlessRaises(IOError, self.audio.delete, fn)
+
+    def test_mime(self):
+        self.failUnless("audio/x-oggflac" in self.audio.mime)
+
+add(TOggFLAC)
+
+NOTFOUND = os.system("tools/notarealprogram 2> {}".format(os.devnull))
+
+have_flac = True
+if os.system("flac 2> {} > {}".format(os.devnull, os.devnull)) == NOTFOUND:
+    have_flac = False
+    print("WARNING: Skipping Ogg FLAC reference tests.")
